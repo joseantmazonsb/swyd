76ef38b5741ace30c9f0c7e9801755db6e610704
Jose Antonio Mazon SB <joseantoniomazon@icloud.com>
2024-05-04 22:09:28 +0200

    add empty favicon

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,7 +5,6 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import React from "react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
